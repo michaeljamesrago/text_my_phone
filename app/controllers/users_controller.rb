c20d262992b2c9bb76f636c6b1cfa50cc48f05ae
Michael Rago <michael.rago@gmail.com>
2022-11-17 15:49:44 -0500

    Update user create action for account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session
-      log_in @user
-      flash[:success] = "You have successfully created your account."
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email for the link to activate your account"
+      redirect_to root_url
     else
       render 'new', status: :unprocessable_entity
     end
